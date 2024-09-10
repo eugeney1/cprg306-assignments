64b80877a19ba30364ef6edc6177b66070f9db6e
Colin Ly <bosscolinly@gmail.com>
2024-09-10 12:59:42 -0600

    week2 assignment

---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "./student-info";
+
+export default function Page() {
+    return (
+      <main>
+        <h1>Shopping List</h1>
+        <h1><StudentInfo></StudentInfo></h1>
+      </main>
+    );
+  }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return(
+        <main>
+            <h1>Colin Ly</h1>
+            <ul>
+            <li><Link href="https://github.com/eugeney1?tab=repositories">https://github.com/eugeney1?tab=repositories</Link></li>
+            </ul>
+        </main>
+    )
+}
