5779f12e150aab9a89502cbe20db9cff9a9a9fc4
Colin Ly <bosscolinly@gmail.com>
2024-11-18 13:58:16 -0700

    fix some stuff

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -32,8 +32,8 @@ export default function SignInPage() {
                 <div>
                     <p>Welcome, {user.displayName || user.email}!</p>
                     {user.photoURL && <img src={user.photoURL} alt="User Profile" />}
-                    <Link href="/week-8/">
-                        <a className="text-blue-500">Week 8</a> {/* Add styling if needed */}
+                    <Link href="/week-9/shopping-list">
+                        <a className="text-blue-500">Week 8</a> {}
                     </Link>
                     <button type="button" className="text-white" onClick={handleSignOut}>Sign Out</button>
                 </div>
